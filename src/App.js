b468d63a3e71fc343b0aa01679ce27b4d71cd381
manuyadav7351 <manuyadav7351@gmail.com>
2023-09-27 22:48:32 +0530

    adding to portfolio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from "./component/header/Header";
 import Trending from "./component/trending/Trending";
 import PopularDrink from "./component/popularDrink/PopularDrink"
 import Aboutus from './component/aboutus/Aboutus';
-import Register from './component/register/Register';
+// import Register from './component/register/Register';
+// import Login from './component/register/Login';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Trending />
       <PopularDrink />
       <Aboutus />
-      <Register />
+      {/* <Register />
+      <Login /> */}
     </div>
   );
 }
--- src/component/register/Login.jsx
+++ src/component/register/Login.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import "login.scss"
+import React from "react";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div className='Login'>
-      <form action="#">
-        <input type="email" placeholder='Email'/>
-        <input type="password" placeholder='Password'/>
-        <button>Login</button>
-      </form>
+    <div className="register">
+      <div className="register-container">
+        <h1>Login Form</h1>
+        <form action="#">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="submit">Login</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
