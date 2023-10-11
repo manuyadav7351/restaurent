8c912e55849117db18b6911e2cab2cd76ed699f7
manuyadav7351 <manuyadav7351@gmail.com>
2023-10-12 00:58:57 +0530

    only view less then 540 px is left to manage

--- src/component/coffeemenu/PopularDrink.jsx
+++ src/component/coffeemenu/PopularDrink.jsx
@@ -1,70 +1,71 @@
-import React, { useRef} from "react";
+import React, { useRef } from "react";
 import "./popularDrink.scss";
-import drink from "../../assets/trending/nathan-dumlao.jpg";
 import { selectAllCoffeeMenuItem } from "../../store/features/coffeeMenu/coffeeMenuSlice";
 import { useSelector } from "react-redux";
 
 const PopularDrink = () => {
+    const coffeeMenu = useSelector(selectAllCoffeeMenuItem);
+    console.log(coffeeMenu);
 
-  const coffeeMenu = useSelector(selectAllCoffeeMenuItem);
-  console.log(coffeeMenu)
+    const drinkConatinerRef = useRef(null);
+    const prev = () => {
+        drinkConatinerRef.current.scrollLeft -= 280;
+    };
+    const next = () => {
+        drinkConatinerRef.current.scrollLeft += 280;
+    };
 
-  const drinkConatinerRef = useRef(null);
-  const prev = () => {
-    drinkConatinerRef.current.scrollLeft -= 280;
-  };
-  const next = () => {
-    drinkConatinerRef.current.scrollLeft += 280;
-  };
+    return (
+        <>
+            <div className="Drinks">
+                <div className="divide">
+                    <div className="div1"></div>
+                    <div className="div2"></div>
+                </div>
+                <div className="drink-data">
+                    <div className="drink-heading">Drinks For You</div>
+                    <div className="drink-slider">
+                        <div
+                            className="drink-container"
+                            ref={drinkConatinerRef}
+                        >
+                            {/* <button onClick={handlePrevious}>Previous</button> */}
+                            {/* cards */}
+                            {coffeeMenu.map((coffee) => (
+                                <div
+                                    className="drink-card-coffee"
+                                    key={coffee.imageUrl}
+                                >
+                                    <div className="drink-card-image">
+                                        <img src={coffee.imageUrl} alt="" />
+                                    </div>
+                                    <div className="drink-card-desc">
+                                        <div className="drink-name">
+                                            {coffee.name}{" "}
+                                            <span>${coffee.price}</span>
+                                        </div>
 
-  return (
-    <>
-      <div className="Drinks">
-        <div className="divide">
-          <div className="div1"></div>
-          <div className="div2"></div>
-        </div>
-        <div className="drink-data">
-          <div className="drink-heading">Drinks For You</div>
-          <div className="drink-slider">
-            <div className="drink-container" ref={drinkConatinerRef}>
-              {/* <button onClick={handlePrevious}>Previous</button> */}
-              {/* cards */}
-              {coffeeMenu.map( (coffee) => (
-                <div className="drink-card" key={coffee.imageUrl}>
-                <img src={coffee.imageUrl} alt="" />
-                <div className="drink-name">
-                  {coffee.name} <span>${coffee.price}</span>
+                                        <div className="drink-order">
+                                            <button>Order Now</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                            {/* cards */}
+                        </div>
+                    </div>
+                    <div className="scroll">
+                        <button className="drink-scroll" onClick={prev}>
+                            PREV
+                        </button>
+                        <button className="drink-scroll" onClick={next}>
+                            NEXT
+                        </button>
+                    </div>
                 </div>
-                <div className="drink-order">
-                  <button>Order Now</button>
-                </div>
-              </div>
-              ))}
-              <div className="drink-card" key={1}>
-                <img src={drink} alt="" />
-                <div className="drink-name">
-                  Latte <span>$23</span>
-                </div>
-                <div className="drink-order">
-                  <button>Order Now</button>
-                </div>
-              </div>
-              {/* cards */}  
             </div>
-          </div>
-          <div className="scroll">
-            <button className="drink-scroll" onClick={prev}>
-              PREV
-            </button>
-            <button className="drink-scroll" onClick={next}>
-              NEXT
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default PopularDrink;
