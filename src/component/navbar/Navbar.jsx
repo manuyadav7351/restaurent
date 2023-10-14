1d785fe76e736441390cc7c53e8e299517fadb2a
manuyadav7351 <manuyadav7351@gmail.com>
2023-10-14 14:54:10 +0530

    working on responsive design

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,36 +1,44 @@
-import React from "react";
-import { FiUserPlus, FiUser } from "react-icons/fi";
+import React, { useEffect, useState } from "react";
+import { FiUserPlus, FiUser, FiArrowDown } from "react-icons/fi";
 import "./navbar.scss";
-import { selectIsLoggedIn, selectAllUser, clearStorage} from "../../store/features/register/registerSlice";
+import { selectIsLoggedIn} from "../../store/features/register/registerSlice";
 import { useSelector } from "react-redux";
 import { logout } from "../../store/features/register/registerSlice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleLogout = () => {
         dispatch(logout());
+        swal({title: "Success!", text: "Logged Out Successfully!", icon: "warning", buttons: false});
+        setTimeout(() => {
+            swal.close();
+        }, 3000);
         navigate('/login')
     };
 
-    const handleStorage = () => {
-        dispatch(clearStorage())
-    }
-    
-    const allUser = useSelector(selectAllUser); 
     const isLoggedIn = useSelector(selectIsLoggedIn);
-    // console.log("all user " ,allUser)
-    // console.log("logged " , isLoggedIn)
+
+
+    const [navVisible, setNavVisible] = useState(false);
+    const handleNavbar = () => {
+        setNavVisible(!navVisible)
+    }
+
 
     return (
         <div className="navbar">
             <div className="navbar-left" onClick={() => navigate("/")}>logo</div>
-            <div className="navbar-right">
+
+            <button className="navbar-hamburger" onClick={() => handleNavbar()}>< FiArrowDown /></button>
+
+            <div className={`navbar-right ${navVisible ? "" : "nope"}`}>
                 <ul>
                     <li onClick={() => navigate("/")}>HOME</li>
-                    <li onClick={() => navigate("/coffeemenu")}>COFFEE MENU</li>
+                    <li onClick={() => navigate("/coffeemenu")}>MENU</li>
                     <li onClick={() => navigate("/weekspecial")}>
                         WEEK SPECIAL
                     </li>
@@ -55,7 +63,7 @@ const Navbar = () => {
                         </>
                     )}
                 </ul>
-            </div>
+            </div> 
         </div>
     );
 };
--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./user.scss";
 import "../pages.scss";
 import Navbar from "../../component/navbar/Navbar";
+import { selectIsLoggedIn } from "../../store/features/register/registerSlice";
+import { useSelector } from "react-redux";
 
 const User = () => {
+
+    const user = useSelector(selectIsLoggedIn);
+
     return (
         <div className="coffee-page">
             <div className="page-navbar">
@@ -11,11 +16,15 @@ const User = () => {
             </div>
             <div className="coffee-page-content">
                 <div className="page-content">
-                    <div className="cart">
-                        <div className="cart-top">
+                    <div className="user">
+                        <div className="user-top">
                             <div className="heading">User Profile</div>
                         </div>
-                        <div className="cart-bottom">
+                        <div className="user-bottom">
+                            <div className="user-bottom-desc">
+                                <div className="user-info">Name : {user.name}</div>
+                                <div className="user-info">Email : {user.email}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
